d5d4be1ff9ad3184ed28b7da664e0df94e3d1fd5
Bart Arys <mysterybtdn@gmail.com>
2017-11-28 17:55:02 +0100

    add semantic

--- frontend/ome/src/app/app.module.ts
+++ frontend/ome/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PersonComponent } from './person/person.component';
+import { EventComponent } from './event/event.component';
+import { AddEventComponent } from './add-event/add-event.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent,
+    EventComponent,
+    AddEventComponent
   ],
   imports: [
     BrowserModule
